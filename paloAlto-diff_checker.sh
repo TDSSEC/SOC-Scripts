1e35157dfe56e88f29488a34eefea9334aa16f49
tomsqr94 <tom_sqr@hotmail.co.uk>
2018-03-01 16:57:49 +0000

    Update paloAlto-diff_checker.sh

--- paloAlto-diff_checker.sh
+++ paloAlto-diff_checker.sh
@@ -1,14 +1,15 @@
 #/bin/bash!
 
 # Helps compare config changes using diff from a Palo Alto config change
-# Created on 01 Mar 2018 by TomSqr94
+# Created on 01 Mar 2018 by TomSqr94 - v1.0
 
 # First we need to enter the two sides to compare
-read -p "Enter the before config: " before
+read -p "Enter the config prior to the change: " before
 read -p "Enter the config after the change: " after
 
 echo $before >> before.txt
 echo $after >> after.txt
+
 # Sort the input into new lines after every space detected
 tr -s ' ' '\n' < before.txt > before2.txt
 tr -s ' ' '\n' < after.txt > after2.txt
